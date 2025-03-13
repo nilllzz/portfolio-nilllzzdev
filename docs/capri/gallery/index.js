a2a4d01e07ba78accbec9203da4313c957638be3
nilllzz <nidresch@googlemail.com>
2025-03-13 13:55:21 +0100

    set theme color, update formatting

--- docs/capri/gallery/index.js
+++ docs/capri/gallery/index.js
@@ -7,167 +7,165 @@
  * @type {GalleryEntry[]} gallery
  */
 const gallery = [
-  {
-    title: "Goober",
-    imgUrl: "goober.png",
-    author: {
-      name: "BlueRhapsodyy",
-      url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+    {
+        title: "Goober",
+        imgUrl: "goober.png",
+        author: {
+            name: "BlueRhapsodyy",
+            url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+        },
     },
-  },
-  {
-    title: "Harness",
-    imgUrl: "harness.png",
-    author: {
-      name: "BlueRhapsodyy",
-      url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+    {
+        title: "Harness",
+        imgUrl: "harness.png",
+        author: {
+            name: "BlueRhapsodyy",
+            url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+        },
     },
-  },
-  {
-    title: "Hug",
-    imgUrl: "hug.png",
-    author: {
-      name: "BlueRhapsodyy",
-      url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+    {
+        title: "Hug",
+        imgUrl: "hug.png",
+        author: {
+            name: "BlueRhapsodyy",
+            url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+        },
     },
-  },
-  {
-    title: "Reference Sheet",
-    imgUrl: "ref.png",
-    author: {
-      name: "BlueRhapsodyy",
-      url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+    {
+        title: "Reference Sheet",
+        imgUrl: "ref.png",
+        author: {
+            name: "BlueRhapsodyy",
+            url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+        },
     },
-  },
-  {
-    title: "Sphere",
-    imgUrl: "sphere.gif",
-    author: {
-      name: "BlueRhapsodyy",
-      url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+    {
+        title: "Sphere",
+        imgUrl: "sphere.gif",
+        author: {
+            name: "BlueRhapsodyy",
+            url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+        },
     },
-  },
-  {
-    title: "Stare",
-    imgUrl: "stare.png",
-    author: {
-      name: "JulianDough",
-      url: "https://juliandoes.art",
+    {
+        title: "Stare",
+        imgUrl: "stare.png",
+        author: {
+            name: "JulianDough",
+            url: "https://juliandoes.art",
+        },
     },
-  },
-  {
-    title: "Wave",
-    imgUrl: "wave.gif",
-    author: {
-      name: "Natezacate",
+    {
+        title: "Wave",
+        imgUrl: "wave.gif",
+        author: {
+            name: "Natezacate",
+        },
     },
-  },
-  {
-    title: "Angy",
-    imgUrl: "angy.png",
-    author: {
-      name: "BlueRhapsodyy",
-      url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+    {
+        title: "Angy",
+        imgUrl: "angy.png",
+        author: {
+            name: "BlueRhapsodyy",
+            url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+        },
     },
-  },
-  {
-    title: "Amogus",
-    imgUrl: "amogus.png",
-    author: {
-      name: "JulianDough",
-      url: "https://juliandoes.art",
+    {
+        title: "Amogus",
+        imgUrl: "amogus.png",
+        author: {
+            name: "JulianDough",
+            url: "https://juliandoes.art",
+        },
     },
-  },
 
-  {
-    title: "Nodding",
-    imgUrl: "capri_bopbop_2_l_square.gif",
-    author: {
-      name: "BlueRhapsodyy",
-      url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+    {
+        title: "Nodding",
+        imgUrl: "capri_bopbop_2_l_square.gif",
+        author: {
+            name: "BlueRhapsodyy",
+            url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+        },
     },
-  },
-  {
-    title: "Flooshed",
-    imgUrl: "flooshed_square.png",
-    author: {
-      name: "BlueRhapsodyy",
-      url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+    {
+        title: "Flooshed",
+        imgUrl: "flooshed_square.png",
+        author: {
+            name: "BlueRhapsodyy",
+            url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+        },
     },
-  },
-  {
-    title: "Hi! Doodle",
-    imgUrl: "racc_doodle.png",
-    author: {
-      name: "ImJustRacc",
+    {
+        title: "Hi! Doodle",
+        imgUrl: "racc_doodle.png",
+        author: {
+            name: "ImJustRacc",
+        },
     },
-  },
-  {
-    title: "RAHHHH!",
-    imgUrl: "rah_square.png",
-    author: {
-      name: "BlueRhapsodyy",
-      url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+    {
+        title: "RAHHHH!",
+        imgUrl: "rah_square.png",
+        author: {
+            name: "BlueRhapsodyy",
+            url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+        },
     },
-  },
-  {
-    title: "Stare...",
-    imgUrl: "stare2_square.png",
-    author: {
-      name: "BlueRhapsodyy",
-      url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+    {
+        title: "Stare...",
+        imgUrl: "stare2_square.png",
+        author: {
+            name: "BlueRhapsodyy",
+            url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+        },
     },
-  },
-  {
-    title: "'tism",
-    imgUrl: "tism_square.png",
-    author: {
-      name: "BlueRhapsodyy",
-      url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+    {
+        title: "'tism",
+        imgUrl: "tism_square.png",
+        author: {
+            name: "BlueRhapsodyy",
+            url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+        },
     },
-  },
-  {
-    title: "WAAAAAHHH *sniff*",
-    imgUrl: "waaaaaahhhh_square.png",
-    author: {
-      name: "BlueRhapsodyy",
-      url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+    {
+        title: "WAAAAAHHH *sniff*",
+        imgUrl: "waaaaaahhhh_square.png",
+        author: {
+            name: "BlueRhapsodyy",
+            url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+        },
     },
-  },
-  {
-    title: "AAAARRRGGHHH",
-    imgUrl: "aaaarrrgghhh.png",
-    author: {
-      name: "BlueRhapsodyy",
-      url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+    {
+        title: "AAAARRRGGHHH",
+        imgUrl: "aaaarrrgghhh.png",
+        author: {
+            name: "BlueRhapsodyy",
+            url: "https://bsky.app/profile/bluerhapsodyy.bsky.social",
+        },
     },
-  },
 ];
 
 const galleryItemTemplate = document.getElementById("galleryItemTemplate");
 const galleryEl = document.getElementById("gallery");
 
-for (const galleryEntry of gallery.toSorted((a, b) =>
-  a.title.localeCompare(b.title)
-)) {
-  const galleryItem = galleryItemTemplate.content.cloneNode(true);
+for (const galleryEntry of gallery.toSorted((a, b) => a.title.localeCompare(b.title))) {
+    const galleryItem = galleryItemTemplate.content.cloneNode(true);
 
-  const imageEl = galleryItem.querySelector('[data-id="image"]');
-  imageEl.src = `../img/gallery/${galleryEntry.imgUrl}`;
-  imageEl.setAttribute("alt", galleryEntry.title);
-  imageEl.setAttribute("title", galleryEntry.title);
+    const imageEl = galleryItem.querySelector('[data-id="image"]');
+    imageEl.src = `../img/gallery/${galleryEntry.imgUrl}`;
+    imageEl.setAttribute("alt", galleryEntry.title);
+    imageEl.setAttribute("title", galleryEntry.title);
 
-  const titleEl = galleryItem.querySelector('[data-id="title"]');
-  titleEl.textContent = galleryEntry.title;
+    const titleEl = galleryItem.querySelector('[data-id="title"]');
+    titleEl.textContent = galleryEntry.title;
 
-  const imageLinkEl = galleryItem.querySelector('[data-id="imageLink"]');
-  imageLinkEl.href = `../img/gallery/${galleryEntry.imgUrl}`;
+    const imageLinkEl = galleryItem.querySelector('[data-id="imageLink"]');
+    imageLinkEl.href = `../img/gallery/${galleryEntry.imgUrl}`;
 
-  const authorEl = galleryItem.querySelector('[data-id="author"]');
-  authorEl.textContent = galleryEntry.author.name;
-  if (galleryEntry.author.url) {
-    authorEl.href = galleryEntry.author.url;
-  }
+    const authorEl = galleryItem.querySelector('[data-id="author"]');
+    authorEl.textContent = galleryEntry.author.name;
+    if (galleryEntry.author.url) {
+        authorEl.href = galleryEntry.author.url;
+    }
 
-  galleryEl.appendChild(galleryItem);
+    galleryEl.appendChild(galleryItem);
 }
--- docs/capri/index.js
+++ docs/capri/index.js
@@ -3,90 +3,87 @@ const detailsDialogCloseEl = document.getElementById("detailsDialogClose");
 
 const allPoiEls = document.querySelectorAll("[data-poi]");
 const allPois = Array.from(allPoiEls)
-  .map((el) => el.getAttribute("data-poi"))
-  .filter((x) => x !== null);
+    .map((el) => el.getAttribute("data-poi"))
+    .filter((x) => x !== null);
 const allPoiTitles = Array.from(allPoiEls)
-  .map((el) => el.getAttribute("title"))
-  .filter((x) => x !== null);
+    .map((el) => el.getAttribute("title"))
+    .filter((x) => x !== null);
 
 let currentPoi = "";
 
 // Show intro modal on first visit.
 if (!localStorage.getItem("visited")) {
-  localStorage.setItem("visited", "1");
-  showDetailsModal("capriInfo");
+    localStorage.setItem("visited", "1");
+    showDetailsModal("capriInfo");
 } else if (window.location.hash) {
-  // Show POI if hash is set in URL.
-  const hash = window.location.hash.substring(1);
-  if (allPois.includes(hash)) {
-    showPoi(hash);
-  }
+    // Show POI if hash is set in URL.
+    const hash = window.location.hash.substring(1);
+    if (allPois.includes(hash)) {
+        showPoi(hash);
+    }
 }
 
 function openDetails(e) {
-  const poiType = e.target.getAttribute("data-poi");
-  if (!poiType) {
-    return;
-  }
+    const poiType = e.target.getAttribute("data-poi");
+    if (!poiType) {
+        return;
+    }
 
-  showPoi(poiType);
+    showPoi(poiType);
 }
 
 function showPoi(poiType) {
-  // Find template for POI and set the content of the dialog.
-  currentPoi = poiType;
-  showDetailsModal(`poi-${poiType}`);
+    // Find template for POI and set the content of the dialog.
+    currentPoi = poiType;
+    showDetailsModal(`poi-${poiType}`);
 
-  // Add nav buttons to the dialog.
-  const navButtonsTemplate = document.getElementById("detailsNavButtons");
-  const navButtonsClone = navButtonsTemplate.content.cloneNode(true);
+    // Add nav buttons to the dialog.
+    const navButtonsTemplate = document.getElementById("detailsNavButtons");
+    const navButtonsClone = navButtonsTemplate.content.cloneNode(true);
 
-  const buttons = navButtonsClone.querySelectorAll("button");
-  const previousPoiTitle =
-    allPoiTitles[
-      (allPois.indexOf(currentPoi) - 1 + allPois.length) % allPois.length
-    ];
-  const nextPoiTitle =
-    allPoiTitles[(allPois.indexOf(currentPoi) + 1) % allPois.length];
-  buttons[0].innerHTML = `<i class="bi bi-arrow-left"></i> ${previousPoiTitle}`;
-  buttons[1].innerHTML = `${nextPoiTitle} <i class="bi bi-arrow-right"></i>`;
+    const buttons = navButtonsClone.querySelectorAll("button");
+    const previousPoiTitle =
+        allPoiTitles[(allPois.indexOf(currentPoi) - 1 + allPois.length) % allPois.length];
+    const nextPoiTitle = allPoiTitles[(allPois.indexOf(currentPoi) + 1) % allPois.length];
+    buttons[0].innerHTML = `<i class="bi bi-arrow-left"></i> ${previousPoiTitle}`;
+    buttons[1].innerHTML = `${nextPoiTitle} <i class="bi bi-arrow-right"></i>`;
 
-  detailsDialogEl.appendChild(navButtonsClone);
+    detailsDialogEl.appendChild(navButtonsClone);
 
-  // Set the hash to the current POI.
-  window.location.hash = currentPoi;
+    // Set the hash to the current POI.
+    window.location.hash = currentPoi;
 }
 
 function showDetailsModal(copyFromId) {
-  const template = document.getElementById(copyFromId);
-  if (!template) {
-    return;
-  }
+    const template = document.getElementById(copyFromId);
+    if (!template) {
+        return;
+    }
 
-  const clone = template.content.cloneNode(true);
-  detailsDialogEl.innerHTML = "";
-  detailsDialogEl.appendChild(clone);
+    const clone = template.content.cloneNode(true);
+    detailsDialogEl.innerHTML = "";
+    detailsDialogEl.appendChild(clone);
 
-  // Add close button:
-  const closeBtnClone = detailsDialogCloseEl.content.cloneNode(true);
-  detailsDialogEl.appendChild(closeBtnClone);
+    // Add close button:
+    const closeBtnClone = detailsDialogCloseEl.content.cloneNode(true);
+    detailsDialogEl.appendChild(closeBtnClone);
 
-  detailsDialogEl.showModal();
+    detailsDialogEl.showModal();
 }
 
 function closeDetailsModal() {
-  window.location.hash = "";
-  detailsDialogEl.close();
+    window.location.hash = "";
+    detailsDialogEl.close();
 }
 
 function viewNextPoi() {
-  const currentIndex = allPois.indexOf(currentPoi);
-  const nextIndex = (currentIndex + 1) % allPois.length;
-  showPoi(allPois[nextIndex]);
+    const currentIndex = allPois.indexOf(currentPoi);
+    const nextIndex = (currentIndex + 1) % allPois.length;
+    showPoi(allPois[nextIndex]);
 }
 
 function viewPrevPoi() {
-  const currentIndex = allPois.indexOf(currentPoi);
-  const nextIndex = (currentIndex - 1 + allPois.length) % allPois.length;
-  showPoi(allPois[nextIndex]);
+    const currentIndex = allPois.indexOf(currentPoi);
+    const nextIndex = (currentIndex - 1 + allPois.length) % allPois.length;
+    showPoi(allPois[nextIndex]);
 }
